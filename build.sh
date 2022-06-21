340fe270b69e3a9085f45dfa3f43f715cbd7ec70
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-06-21 21:49:19 +0800

    refactor: moved www to docs

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown --release
-cp  -f ./target/wasm32-unknown-unknown/release/dogchicken.wasm ./www/dogchicken.wasm
+cp  -f ./target/wasm32-unknown-unknown/release/dogchicken.wasm ./docs/dogchicken.wasm
